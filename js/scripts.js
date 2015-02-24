b9973b465d4a1c8ec96a1a07e5083a286affe0b3
Chris Szeluga <chris.szeluga@yahoo.com>
2015-02-23 22:38:40 -0500

    demo regions json

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+
+
+    // Get URL Paramater
+    function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1);
+        var sURLVariables = sPageURL.split('&');
+        for (var i = 0; i < sURLVariables.length; i++) {
+            var sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] == sParam) {
+                return sParameterName[1];
+            }
+        }
+    }
+
+    // Load JSON File
+    function loadJSON(callback) {
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', '../js/regions.json', true);
+        xobj.onreadystatechange = function() {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+                callback(xobj.responseText);
+            }
+        };
+        xobj.send(null);
+    }
+
+    // Show region stats based on parameter
+    var regionAbb = getUrlParameter('region');
+    var regionTitle;
+    var regionThreshold;
+
+    loadJSON(function(response) {
+        // Parse JSON string into object
+        var data = $.parseJSON(response);
+
+        console.log(data);
+
+        regionTitle = data.daocloudRegionThresholds.regions[regionAbb] || 'local';
+        regionThreshold = data.daocloudRegionThresholds.thresholds[regionAbb] || 'a minimum number of';
+
+        console.log(regionTitle);
+
+        $('.regionTitle').html(regionTitle);
+        $('.regionThreshold').html(regionThreshold);
+    });
+
+
+});
